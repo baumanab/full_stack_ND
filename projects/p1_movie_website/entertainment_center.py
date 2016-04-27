0ef273879ccfcc8b1c7c70cd105b513fde410df1
Andrew Bauman <baumanab@gmail.com>
2016-04-27 13:37:24 -0700

    Update entertainment_center.py

--- projects/p1_movie_website/entertainment_center.py
+++ projects/p1_movie_website/entertainment_center.py
@@ -22,16 +22,33 @@ Move Parameters:
 import media
 import fresh_tomatoes
 
-full_metal_jacket = media.Movie("Full Metal Jacket", "A marine journalist during the vietnam conflict",
+full_metal_jacket = media.Movie("Full Metal Jacket", 
+                                "A marine journalist during the vietnam conflict",
+                                 "(1987)",
                                 "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg", 
-                                "https://www.youtube.com/watch?v=x9f6JaaX7Wg")
+                                "https://www.youtube.com/watch?v=x9f6JaaX7Wg",
+                                "[Drama, Action & Adventure]",
+                                "95% Fresh",
+                                "Compelling")
 
 
 the_princess_bride = movie.Movie("The Princess Bride",
                                  "(1987)",
                                  "Boy from boy meets world meets book about boy meets girl",
                                  "http://ia.media-imdb.com/images/M/MV5BMTkzMDgyNjQwM15BMl5BanBnXkFtZTgwNTg2Mjc1MDE@._V1_SY317_CR0,0,214,317_AL_.jpg",
-                                 "https://www.youtube.com/watch?v=VYgcrny2hRs")
+                                 "https://www.youtube.com/watch?v=VYgcrny2hRs",
+                                 "[Action & Adventure, Kids & Family, Romance, Science Fiction & Fantasy, Comedy]",
+                                 "97% Fresh",
+                                 "As you wish")
+
+blast_from_past = movie.Movie("Blast From The Past",
+                                 "(1999)",
+                                 "Girl meets boy from bomb shelter ",
+                                 "https://www.movieposter.com/posters/archive/main/97/MPW-48881",
+                                 "https://youtu.be/AhMQOb0tEmI",
+                                 "[Comedy, Romance]",
+                                 "58% Fresh",
+                                 "Enjoyable")
 
 # list to feed to movie web page generator
 movies = [
