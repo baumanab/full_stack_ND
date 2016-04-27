13534a674126f3e175330beaeea2038232e18b6a
Andrew Bauman <baumanab@gmail.com>
2016-04-27 13:22:02 -0700

    Update attributes

--- projects/p1_movie_website/media.py
+++ projects/p1_movie_website/media.py
@@ -15,15 +15,17 @@ class Movie():
     '''
     This class provides a way to store movie attributes and information.
     '''
-
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
+    
+    def __init__(self, movie_title, release_year,movie_storyline, poster_image,
+                 trailer_youtube, genre, rott_tom_rating, my_rating):
         self.title = movie_title
+        self.release_year = release_year
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        self.genre = genre
+        self.rott_tom_rating = rott_tom_rating # Rotten Tomatoes rating
+        self.my_rating = my_rating
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
