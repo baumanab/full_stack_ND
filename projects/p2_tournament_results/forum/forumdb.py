8deaf8ffb3ccd8ffd1c0ed5b62d04a97005676a2
baumanab <baumanab@gmail.com>
2016-05-13 22:10:38 -0700

    clean up merge

--- projects/p2_tournament_results/forum/forumdb.py
+++ projects/p2_tournament_results/forum/forumdb.py
@@ -4,18 +4,17 @@
 
 import psycopg2
 
-<<<<<<< HEAD
 
 def GetAllPosts():
     '''Get all the posts from the database, sorted with the newest first.
     '''
 
-=======
+
 ## Get posts from database.
 def GetAllPosts():
     '''Get all the posts from the database, sorted with the newest first.
     '''
->>>>>>> 1b7fb009434c688131048692c11ce2cfb2934c17
+
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
     query = "SELECT time, content FROM posts ORDER BY time DESC"
@@ -33,16 +32,11 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-<<<<<<< HEAD
+
 
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    query = "INSERT INTO posts (content) VALUES ({}))".format(content)
-=======
-    DB = psycopg2.connect("dbname=forum")
-    c = DB.cursor()
     query = "INSERT INTO posts (content) VALUES ('{}'))".format(content)
->>>>>>> 1b7fb009434c688131048692c11ce2cfb2934c17
     c.execute(query)
     DB.commit()
     DB.close()
