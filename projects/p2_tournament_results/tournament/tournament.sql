73e737f531a5e6c6895d7e5d1ac1182fb25a4e3f
baumanab <baumanab@gmail.com>
2016-05-19 13:49:09 -0700

    update players table and matches table

--- projects/p2_tournament_results/tournament/tournament.sql
+++ projects/p2_tournament_results/tournament/tournament.sql
@@ -1,9 +1,19 @@
 -- Table definitions for the tournament project.
 --
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+-- players table: Players registered for tournament
+-- field 1: name {text} name of a player
+-- field 2: id {serial} unique id of a player
+
+CREATE TABLE players ( name TEXT,
+                       id SERIAL PRIMARY KEY );
+
+-- matches table: Match results for tournament
+--- field 1: match id (serial) unique id of the match played
+-- field 2: result {real} result of a match. Winner = 1, Loser = 0
+-- field 3: id {integer} player_id of a player, ties to id field of players table.
+
+CREATE TABLE matches ( match_id SERIAL PRIMARY KEY,
+                       result REAL,
+                       FOREIGN KEY (player_id) REFERENCES players(id) );
 
