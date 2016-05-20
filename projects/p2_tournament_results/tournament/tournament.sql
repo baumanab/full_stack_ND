f9fb85fba6db33aa6a6668cc207b7a7dd6137816
baumanab <baumanab@gmail.com>
2016-05-19 22:01:56 -0700

    add create database, complete matches table

--- projects/p2_tournament_results/tournament/tournament.sql
+++ projects/p2_tournament_results/tournament/tournament.sql
@@ -1,3 +1,6 @@
+-- Create the database for the tournament project
+CREATE DATABASE tournament; 
+
 -- Table definitions for the tournament project.
 --
 
@@ -9,11 +12,12 @@ CREATE TABLE players ( name TEXT,
                        id SERIAL PRIMARY KEY );
 
 -- matches table: Match results for tournament
---- field 1: match id (serial) unique id of the match played
+-- field 1: match id (serial) unique id of the match played
 -- field 2: result {real} result of a match. Winner = 1, Loser = 0
 -- field 3: id {integer} player_id of a player, ties to id field of players table.
 
 CREATE TABLE matches ( match_id SERIAL PRIMARY KEY,
                        result REAL,
+					   player_id SERIAL,
                        FOREIGN KEY (player_id) REFERENCES players(id) );
 
